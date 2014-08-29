d3c2c38e91c107cb21d06a146b8c29ac5a9d4845
hAWKdv <hawk94@abv.bg>
2014-08-29 13:21:33 +0300

    Added MongoDB models and client app
    Note:
    - The code hasn't been properly tested yet.
    - Changes are highly possible.

---
+++ PitFiend/SexStore.MongoDB.Models/ProductCategory.cs
@@ -0,0 +1,23 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace SexStore.MongoDB.Models
+{
+    public class ProductCategory
+    {
+        [BsonRequired]
+        public ObjectId ProductId { get; set; }
+        public Product Product { get; set; }
+
+        [BsonRequired]
+        public ObjectId CategoryId { get; set; }
+        public Category Category { get; set; }
+
+        [BsonConstructor]
+        public ProductCategory(ObjectId productId, ObjectId categoryId)
+        {
+            this.ProductId = productId;
+            this.CategoryId = categoryId;
+        }
+    }
+}
