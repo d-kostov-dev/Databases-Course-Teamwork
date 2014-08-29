996cd0049a275d1110864941d55bb914fdf6579c
Dimitar Kostov <d.kostov.dev@gmail.com>
2014-08-29 19:54:41 +0300

    SQLite Files

---
+++ PitFiend/SQLiteServer.Data/ProductInfo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace SQLiteServer.Data
+{
+    public class ProductInfo
+    {
+        public ProductInfo(string productName, int productTax)
+        {
+            this.ProductName = productName;
+            this.TaxPercent = productTax;
+        }
+
+        public string ProductName { get; set; }
+
+        public int TaxPercent { get; set; }
+    }
+}
---
+++ PitFiend/SQLiteServer.Data/SQLiteServer.cs
@@ -0,0 +1,42 @@
+﻿namespace SQLiteServer.Data
+{
+    using System.Collections.Generic;
+    using System.Data.SQLite;
+
+    public class SQLiteServer
+    {
+        private SQLiteConnection sqLiteConnection;
+
+        public SQLiteServer(string connectionString)
+        {
+            this.sqLiteConnection = new SQLiteConnection(connectionString);
+        }
+
+        public ICollection<ProductInfo> GetProductsInformation()
+        {
+            var reportsToReturn = new List<ProductInfo>();
+
+            this.sqLiteConnection.Open();
+
+            using (sqLiteConnection)
+            {
+                string sqlCommand = "SELECT * FROM productsInfo";
+                var commandToExecute = new SQLiteCommand(sqlCommand, this.sqLiteConnection);
+                var reader = commandToExecute.ExecuteReader();
+
+                using (reader)
+                {
+                    while (reader.Read())
+                    {
+                        string productName = (string)reader["product_name"];
+                        int productTax = (int)reader["roduct_tax"];
+                        var currentProductInfo = new ProductInfo(productName, productTax);
+                        reportsToReturn.Add(currentProductInfo);
+                    }
+                }
+            }
+
+            return reportsToReturn;
+        }
+    }
+}
