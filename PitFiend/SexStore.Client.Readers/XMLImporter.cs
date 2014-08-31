1c517827a3540eb989ef8663cea1792ba2b916f4
Kristiyan Petrov <kris4o1993@gmail.com>
2014-08-31 11:28:13 +0300

    xmp importer

---
+++ PitFiend/SexStore.Client.Readers/XMLImporter.cs
@@ -0,0 +1,46 @@
+﻿namespace SexStore.Client.Readers
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Xml.Linq;
+
+    public class XMLImporter
+    {
+        public static List<Tuple<string, string, string>> GetObjects(string fileName)
+        {
+            List<Tuple<string, string, string>>
+            expenses = new List<Tuple<string, string, string>>();
+
+            XDocument xmlDoc = XDocument.Load(fileName);
+
+            foreach (var sale in xmlDoc.Descendants("sale"))
+            {
+                string vendorName = sale.Attribute("vendor").Value;
+
+                foreach (var expense in sale.Descendants("expenses"))
+                {
+                    string month = expense.Attribute("month").Value;
+                    string value = expense.Value;
+
+                    expenses.Add(new Tuple<string, string, string>(vendorName, month, value));
+                }
+            }
+
+            return expenses;
+        }
+
+      //  public static void ImportExpensesReportsFromXml()
+      //  {
+      //      var expenses = GetObjects("..\\..\\..\\Vendors-Expenses.xml");
+      //      foreach (var expense in expenses)
+      //      {
+      //          newExpense.VendorName = expense.Item1;
+      //          newExpense.Month = expense.Item2;
+      //          newExpense.Value = expense.Item3;
+      //
+      //          Database.SaveData(newExpense);
+      //      }
+      //  }
+    }
+}
