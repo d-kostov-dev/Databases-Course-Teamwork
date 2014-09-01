e1ec305c496bd8ee977b713191c52645a1342ee4
georgipyaramov <georgipyaramov@gmail.com>
2014-09-01 12:57:04 +0300

    Fixed missing ProductReport.cs

---
+++ PitFiend/SexStore.Client.Readers/Helpers/ProductReport.cs
@@ -0,0 +1,31 @@
+﻿namespace SexStore.Client.Readers.Helpers
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class ProductReport
+    {
+        private List<string> shopNames;
+
+        public ProductReport()
+        {
+            this.shopNames = new List<string>();
+        }
+
+        public int Id { get; set; }
+
+        public int ProductCode { get; set; }
+
+        public string Name { get; set; }
+
+        public List<string> ShopNames 
+        {
+            get { return this.shopNames; }
+        }
+
+        public int TotalQuantitySold { get; set; }
+
+        public double TotalIncomes { get; set; }
+    }
+}
