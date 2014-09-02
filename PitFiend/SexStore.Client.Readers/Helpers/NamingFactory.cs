50fec95cff483918ba493660be1a0ee104859351
hAWKdv <hawk94@abv.bg>
2014-09-02 14:31:59 +0300

    Merge Conflict in Helpers/NamingFactory.cs

--- PitFiend/SexStore.Client.Readers/Helpers/NamingFactory.cs
+++ PitFiend/SexStore.Client.Readers/Helpers/NamingFactory.cs
@@ -9,18 +9,21 @@
         public static string BuildName(string methodName, string fileType)
         {
             // Removed const since you cannot assign a constant with DateTime - Georgi
-            string DateTimeFormatType = DateTime.Now.ToString("yyyy-MM-dd-HH-mm-ss");
+            // string DateTimeFormatType = DateTime.Now.ToString("yyyy-MM-dd-HH-mm-ss");
+            string dateTimeFormatType = DateTime.Now.ToString("yyyy-MM-dd-HH-mm-ss");
 
             string tempFolder = string.Format("../../../Reports/{0}Reports/", fileType.ToUpper());
-            string tempFileName = string.Format("{0}-{1}.{2}", methodName, DateTimeFormatType, fileType);
+            string tempFileName = string.Format("{0}-{1}.{2}", methodName, dateTimeFormatType, fileType);
             bool ifExists = Directory.Exists(tempFolder);
 
             if (!ifExists)
             {
                 Directory.CreateDirectory(tempFolder);
             }
-            
-            Console.WriteLine("Example quantities exported to {0}", fileType.ToUpper());
+
+            Console.WriteLine("-------------------------------------------------");
+            Console.WriteLine("Report exported to {0}", fileType.ToUpper());
+            Console.WriteLine("-------------------------------------------------");
             return tempFolder + tempFileName;
         }
     }
